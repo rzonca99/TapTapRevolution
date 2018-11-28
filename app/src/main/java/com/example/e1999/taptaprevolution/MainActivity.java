4df75dfe47f1fd9ff62b25b29eccf62e2ade48e8
rzonca99 <erzonca2@illinois.edu>
2018-11-27 20:38:27 -0600

    Initial commit

---
+++ app/src/main/java/com/example/e1999/taptaprevolution/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.e1999.taptaprevolution;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import java.lang.System;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
