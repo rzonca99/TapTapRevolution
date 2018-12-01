91ff9cb8c9030918dd734c8169b94bb78ef86094
rzonca99 <erzonca2@illinois.edu>
2018-12-01 14:02:59 -0600

    Began work on making our "start" button take the player to the game activity.

--- app/src/main/java/com/example/e1999/taptaprevolution/Conductor.java
+++ app/src/main/java/com/example/e1999/taptaprevolution/Conductor.java
@@ -1,6 +1,18 @@
 package com.example.e1999.taptaprevolution;
 
-public class Conductor {
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import java.lang.System;
+import android.widget.Button;
+
+public class Conductor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_conductor);
+    }
+
     /**
      * the songs bpm
      */
@@ -23,5 +35,10 @@ public class Conductor {
     public int misses = 0;
 
     public int timer = 0;
+
+    /*
+    We probably need a class that contains the video and a function that will launch it, that can
+    be called by the start button.
+     */
 
 }
--- app/src/main/java/com/example/e1999/taptaprevolution/MainActivity.java
+++ app/src/main/java/com/example/e1999/taptaprevolution/MainActivity.java
@@ -2,13 +2,20 @@ package com.example.e1999.taptaprevolution;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+
 import java.lang.System;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(TAG, "onCreate: Starting");
     }
+
 }
