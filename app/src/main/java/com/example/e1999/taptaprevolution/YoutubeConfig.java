2c81a6ac4a524a9bc136e3453232d8836c3ab504
mzhan25@illinois.edu <Theyare,honey.35>
2018-12-03 20:47:40 -0600

    abt half the youtube api

---
+++ app/src/main/java/com/example/e1999/taptaprevolution/YoutubeConfig.java
@@ -0,0 +1,11 @@
+package com.example.e1999.taptaprevolution;
+
+public class YoutubeConfig {
+    public YoutubeConfig() {
+    }
+    private static final String API_KEY = "AIzaSyAruuYENK97xqjTtK2dh1txPDNk-hxsoA0";
+
+    public static String getApiKey() {
+        return API_KEY;
+    }
+}
