a9947cd0e38777213273e40feebf9e54190d93dc
rzonca99 <erzonca2@illinois.edu>
2018-12-05 15:03:30 -0600

    YouTube API info has not broken the code, but it has broken the app. It's commented out for now. UI works just fine.

--- app/src/main/java/com/example/e1999/taptaprevolution/Conductor.java
+++ app/src/main/java/com/example/e1999/taptaprevolution/Conductor.java
@@ -10,9 +10,22 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+/*
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
+*/
+
 public class Conductor extends AppCompatActivity {
 
+    //Important note: all code related to the YouTube API is commented out for now.
+
     private static final String TAG = "Conductor";
+    /*
+    YouTubePlayerView mYouTubePlayerView;
+    YouTubePlayer.OnInitializedListener mOnInitializedListener;
+    */
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,7 +49,33 @@ public class Conductor extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        /*
+        mYouTubePlayerView = (YouTubePlayerView) findViewById(R.id.songPlayer);
+        mYouTubePlayerView.initialize(YoutubeConfig.getApiKey(), mOnInitializedListener);
+        mOnInitializedListener = new YouTubePlayer.OnInitializedListener() {
+            @Override
+            public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+                Log.d(TAG, "onInitializationSuccess: Done initializing video");
+                youTubePlayer.loadVideo("18nDrsoii5M");
+            }
+
+            @Override
+            public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+                Log.d(TAG, "onInitializationFailure: Failed to initialize video");
+
+            }
+        };
+        */
     }
+
+
+    /**
+     * Possible plan of attack: create a function that requires players to hit the button within the
+     * given time frame in order to gain points, as planned. THEN implement a Timer object and
+     * have it call the function at given intervals. That should make the app more versatile and
+     * efficient, as we could theoretically include more songs as long as we examine their lengths
+     * and call the function at the right times.
+     */
 
     /**
      * the song's bpm
