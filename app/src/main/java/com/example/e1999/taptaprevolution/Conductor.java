e3d6dced1e932bc9628f0704d2da5089c3fadaba
rzonca99 <erzonca2@illinois.edu>
2018-12-12 14:26:25 -0600

    Worked on establishing timer points

--- app/src/main/java/com/example/e1999/taptaprevolution/Conductor.java
+++ app/src/main/java/com/example/e1999/taptaprevolution/Conductor.java
@@ -9,6 +9,7 @@ import java.util.Locale;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.google.android.youtube.player.YouTubeBaseActivity;
@@ -40,8 +41,8 @@ public class Conductor extends YouTubeBaseActivity {
         setContentView(R.layout.activity_conductor);
         mTextViewCountDown = findViewById(R.id.text_view_countdown);
         final TextView pointsView = (TextView) findViewById(R.id.hitCount);
-        Button leftButt = (Button) findViewById(R.id.leftButt);
-        Button rightButt = (Button) findViewById(R.id.rightButt);
+        ImageButton leftButt = (ImageButton) findViewById(R.id.leftButt);
+        ImageButton rightButt = (ImageButton) findViewById(R.id.rightButt);
         Button backButt = (Button) findViewById(R.id.backButt);
         leftButt.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -125,22 +126,108 @@ public class Conductor extends YouTubeBaseActivity {
         mTextViewCountDown.setText(timeLeftFormatted);
     }
     public boolean leftTimerPoint() {
-        if (mTimeLeftInMillis < 117000 && mTimeLeftInMillis > 116000) {
+        if (mTimeLeftInMillis < 115800 && mTimeLeftInMillis > 114900) {
             return true;
         }
-        if (mTimeLeftInMillis <= 116000 && mTimeLeftInMillis >= 114500) {
-            return false;
-        }
-        if (mTimeLeftInMillis < 115000 && mTimeLeftInMillis > 113600) {
+        if (mTimeLeftInMillis < 113800 && mTimeLeftInMillis > 112900) {
             return true;
-        } else {
+        }
+        if (mTimeLeftInMillis < 111700 && mTimeLeftInMillis > 110000) {
+            return true;
+        }
+        if (mTimeLeftInMillis < 109800 && mTimeLeftInMillis > 109000) {
+            return true;
+        }
+        if (mTimeLeftInMillis < 108999 && mTimeLeftInMillis > 108000) {
+            return true;
+        }
+        if (mTimeLeftInMillis < 107999 && mTimeLeftInMillis > 107000) {
+            return true;
+        }
+        if (mTimeLeftInMillis < 102999 && mTimeLeftInMillis > 102000) {
+            return true;
+        }
+        if (mTimeLeftInMillis < 101999 && mTimeLeftInMillis > 101000) {
+            return true;
+        }
+        if (mTimeLeftInMillis < 100999 && mTimeLeftInMillis > 100000) {
+            return true;
+        }
+        if (mTimeLeftInMillis < 99999 && mTimeLeftInMillis > 90000) {
+            return true;
+        }
+        if (mTimeLeftInMillis < 87500 && mTimeLeftInMillis > 87301) {
+            return true;
+        }
+        if (mTimeLeftInMillis < 87300 && mTimeLeftInMillis > 87000) {
+            return true;
+        }
+        if (mTimeLeftInMillis < 87100 && mTimeLeftInMillis > 86000) {
+            return true;
+        }
+        else {
             return false;
         }
     }
     public boolean rightTimerPoint() {
-        if (mTimeLeftInMillis < 115000 && mTimeLeftInMillis > 113600) {
+        if (mTimeLeftInMillis < 109000 && mTimeLeftInMillis > 108000) {
             return true;
-        } else {
+        }
+        if (mTimeLeftInMillis < 107200 && mTimeLeftInMillis > 106200) {
+            return true;
+        }
+        if (mTimeLeftInMillis < 176000 && mTimeLeftInMillis > 161000) {
+            return true;
+        }
+        if (mTimeLeftInMillis < 241000 && mTimeLeftInMillis > 226000) {
+            return true;
+        }
+        if (mTimeLeftInMillis < 251000 && mTimeLeftInMillis > 236000) {
+            return true;
+        }
+        if (mTimeLeftInMillis < 265000 && mTimeLeftInMillis > 260000) {
+            return true;
+        }
+        if (mTimeLeftInMillis < 275000 && mTimeLeftInMillis > 270000) {
+            return true;
+        }
+        if (mTimeLeftInMillis < 287500 && mTimeLeftInMillis > 277500) {
+            return true;
+        }
+        if (mTimeLeftInMillis < 320000 && mTimeLeftInMillis > 310000) {
+            return true;
+        }
+        if (mTimeLeftInMillis < 335000 && mTimeLeftInMillis > 325000) {
+            return true;
+        }
+        if (mTimeLeftInMillis < 345000 && mTimeLeftInMillis > 340000) {
+            return true;
+        }
+        if (mTimeLeftInMillis < 355000 && mTimeLeftInMillis > 345000) {
+            return true;
+        }
+        if (mTimeLeftInMillis < 395000 && mTimeLeftInMillis > 385000) {
+            return true;
+        }
+        if (mTimeLeftInMillis < 445000 && mTimeLeftInMillis > 440000) {
+            return true;
+        }
+        if (mTimeLeftInMillis < 455000 && mTimeLeftInMillis > 445000) {
+            return true;
+        }
+        if (mTimeLeftInMillis < 467500 && mTimeLeftInMillis > 457500) {
+            return true;
+        }
+        if (mTimeLeftInMillis < 515000 && mTimeLeftInMillis > 510000) {
+            return true;
+        }
+        if (mTimeLeftInMillis < 530000 && mTimeLeftInMillis > 520000) {
+            return true;
+        }
+        if (mTimeLeftInMillis < 600000 && mTimeLeftInMillis > 590000) {
+            return true;
+        }
+        else {
             return false;
         }
     }
